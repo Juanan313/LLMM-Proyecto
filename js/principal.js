c52ae1725b3508f3a672adf72c037c7ccd80fe9d
Juanan <juanan.pujals@gmail.com>
2018-01-30 14:53:15 +0100

    Func principal

--- js/principal.js
+++ js/principal.js
@@ -15,6 +15,7 @@ function openSideBar() {
     button.style.display = "none";
 }
 
+// Abrir y cerrar el menu navegador y el titulo 
 function openCloseNav() {
     element = document.getElementsByClassName("menunav");
     menu = document.getElementById("menu");
@@ -29,3 +30,17 @@ function openCloseNav() {
         title.style.display = "block";
     }
 }
+
+// funciones alert al clicar en los elementos del footer
+
+var contact = "juanan.pujals@gmail.com"
+    var about = "Gran parte de la información mostrada en esta página ha sido extraida de páginas relacionada con el mismo juego como: \\n whttps://worldofwarcraft.com/es-es/ \\n  http://www.wowchakra.com/ \\n http://es.wowhead.com/"
+    var help = "Mmmm lo siento no lo he entendido bien, ¿para qué se supone que necesitas ayuda?"
+    var privacy = "Gran parte de el contenido de esta página no es de mi propieda y no ha sido utilizado con animo de lucro, tan solo con fines lúdicos."
+
+ 
+function footerExpandInfo(id) {
+    
+
+    alert(id);
+}    
