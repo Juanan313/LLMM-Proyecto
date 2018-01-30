8761f1667abe98d20061a8044fc38a0b2b9cc26d
Juanan <juanan.pujals@gmail.com>
2018-01-30 12:00:04 +0100

    Clases organizado y funciones

--- js/principal.js
+++ js/principal.js
@@ -14,3 +14,18 @@ function openSideBar() {
     button = document.getElementById("right");
     button.style.display = "none";
 }
+
+function openCloseNav() {
+    element = document.getElementsByClassName("menunav");
+    menu = document.getElementById("menu");
+    title = document.getElementById("title");
+    if (element[0].style.display != "none"){
+        element[0].style.display = "none";
+        menu.style.display = "block";
+        title.style.display = "none";
+    } else {
+        element[0].style.display = "block";
+        menu.style.display = "none";
+        title.style.display = "block";
+    }
+}
